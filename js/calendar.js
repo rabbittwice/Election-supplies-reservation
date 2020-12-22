510a23d2dcb52a2d0fda1f156c9a5353c798e9bb
rabbittwice <49359936+rabbittwice@users.noreply.github.com>
2020-12-23 08:48:31 +0900

    Create a calendar based on date

--- js/calendar.js
+++ js/calendar.js
@@ -1,38 +1,66 @@
-(function() {
+(function () {
     const today = new Date();
     const nowYear = today.getFullYear();
     const nowMonth = today.getMonth() + 1;
     const firstDay = new Date(nowYear, nowMonth - 1, 1);
     const firstDayDate = firstDay.getDate();
     const firstDayOfWeek = firstDay.getDay();
-    const lastDay = new Date(nowYear, nowMonth - 1, 0);
+    const lastDay = new Date(nowYear, nowMonth, 0);
     const lastDayDate = lastDay.getDate();
 
     function _makeCalendar() {
         const calTable = document.querySelector('.calTable');
-        const tableRow = document.createElement('tr');
-        const tableDate 
-        = '<td class="dateColumn"></td>' + '<td class="dateColumn"></td>' + '<td class="dateColumn"></td>' + '<td class="dateColumn"></td>' + '<td class="dateColumn"></td>' + '<td class="dateColumn"></td>' 
-        + '<td class="dateColumn"></td>';
-    
-        tableRow.classList.add('dateRow');
-        
-        tableRow.innerHTML= tableDate;
-        
-        let Date = 1;
-    
-        for(let firstWeekDay = 0; firstWeekDay <= 6; firstWeekDay++){
+        const tableRow = [document.createElement('tr'), document.createElement('tr'), document.createElement('tr'), document.createElement('tr'), document.createElement('tr'), document.createElement('tr')];
+
+        const tableDate = '<td class="dateColumn"></td>' + '<td class="dateColumn"></td>' + '<td class="dateColumn"></td>' + '<td class="dateColumn"></td>' + '<td class="dateColumn"></td>' + '<td class="dateColumn"></td>' + '<td class="dateColumn"></td>';
+
+        tableRow[0].classList.add('dateRow');
+
+        tableRow[0].innerHTML = tableDate;
+
+        let date = 1;
+        let weekDay = 0;
+
+        for (weekDay = 0; weekDay <= 6; weekDay++) {
             // 첫째주 부분
-            if(firstWeekDay >= firstDayOfWeek){
-                tableRow.childNodes[firstWeekDay].innerHTML
-                = '<p class="date">' + Date + '</p>';
-                Date++;
+            if (weekDay >= firstDayOfWeek && weekDay < 6) {
+                tableRow[0].childNodes[weekDay].innerHTML = '<p class="date">' + date + '</p>';
+                date++;
             }
-            if(firstWeekDay == 6){
-                calTable.append(tableRow);
+            if (weekDay == 6) {
+                tableRow[0].childNodes[weekDay].innerHTML = '<p class="date">' + date + '</p>';
+                date++;
+                calTable.appendChild(tableRow[0]);
+                weekDay = 0;
+                break;
             }
         };
+
+        for (let i = 1; i <= 4; i++) {
+            tableRow[i].classList.add('dateRow');
+            tableRow[i].innerHTML = tableDate;
+            for (let weekDay = 0; weekDay <= 6; weekDay++) {
+                if (date <= lastDayDate){
+                    tableRow[i].childNodes[weekDay].innerHTML = '<p class="date">' + date + '</p>';
+                    date++;
+                }
+            }
+            calTable.append(tableRow[i]);
+        }
+        
+        if (firstDayOfWeek >= 5) {
+            tableRow[5].classList.add('dateRow');
+            tableRow[5].innerHTML = tableDate;
+            for (let weekDay = 0; weekDay <= 6; weekDay++) {
+                if (date <= lastDayDate){
+                    tableRow[5].childNodes[weekDay].innerHTML = '<p class="date">' + date + '</p>';
+                    date++;
+                }
+            }
+            calTable.append(tableRow[5]);   
+        }
     }
+    
     _makeCalendar();
 
     _showYearMonth();
@@ -45,7 +73,7 @@
         calMonth.innerHTML = nowMonth;
     };
 
-    
-    
+
+
 
 }())
